95f708e2327e3a3b9127cc45e46ad38364e6a4bf
Luiz Eduardo <luizeduardokowalski@gmail.com>
2012-12-04 23:13:40 -0200

    Atualizacoes

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,23 @@ Ielt::Application.routes.draw do
   get "home/index"
 
   root :to => 'home#index'
-  
+
   # Login e logout
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "usuarios#new", :as => "signup"
-  
+
   match "painel" => "painel#index"
-  
+
   #Paginas estaticas
   match "sobre" => "home#sobre"
   match "nossa-visao" => "home#visao"
   match "contato" => "home#contato"
-  
+  match "o-que-cremos" => "home#cremos"
+  match "estrutura" => "home#estrutura"
+  match "historico" => "home#historico"
+  match "estatuto" => "home#estatuto"
+  match "regimento" => "home#regimento"
+
   resources :sessions, :usuarios, :noticias, :paginas, :ministerios, :missionarios, :educacionais
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,33 @@ p2.titulo = "Nossa visao"
 p2.conteudo = "Nossa visao"
 p2.uniq = "visao"
 p2.save
+
+p3 = Pagina.new
+p3.titulo = "O que cremos"
+p3.conteudo = "O que cremos"
+p3.uniq = "cremos"
+p3.save
+
+p4 = Pagina.new
+p4.titulo = "Estrutura"
+p4.conteudo = "Estrutura"
+p4.uniq = "estrutura"
+p4.save
+
+p = Pagina.new
+p.titulo = "Historico"
+p.conteudo = "Historico"
+p.uniq = "historico"
+p.save
+
+p = Pagina.new
+p.titulo = "Estatuto"
+p.conteudo = "Estatuto"
+p.uniq = "estatuto"
+p.save
+
+p = Pagina.new
+p.titulo = "Regimento"
+p.conteudo = "Regimento"
+p.uniq = "regimento"
+p.save
